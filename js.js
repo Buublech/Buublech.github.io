d85e8bc4126b4c50637eb3986cc7fc818d695d34
Buublech <ibullat@gmail.com>
2025-03-09 15:57:27 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Да есть, бро)')
+	alert('https://sochi.hh.ru/resume/ac24082cff0e87faa80039ed1f41643538594d')
 })
